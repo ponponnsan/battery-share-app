6cf535f774bb7a40b753901c0c84768841ba370c
ponponnsan <2469suhara@gmail.com>
2024-10-04 08:34:00 +0900

    ajust another component

--- frontend/src/app/driver-pickup/page.tsx
+++ frontend/src/app/driver-pickup/page.tsx
@@ -1,8 +1,7 @@
 "use client"
-import Map from "@/components/maps/google-map";
+import MapComponent from "@/components/maps/open-maps";
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { LoadScript } from '@react-google-maps/api';
 import { MapPin, Phone } from 'lucide-react';
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from 'react';
@@ -11,6 +10,8 @@ const CommuteCargoPickupNavigation = () => {
   const [estimatedTime, setEstimatedTime] = useState('15 min');
   const [distance, setDistance] = useState('3.2 km');
   const router = useRouter();
+  const [startPoint, setStartPoint] = useState<RoutePoint | null>(null);
+  const [endPoint, setEndPoint] = useState<RoutePoint | null>(null);
   const [user, setUser] = useState({ name: '' });
   const [UserPickup, setUserPickup] = useState({   
     pickupLocation: '',
@@ -31,6 +32,13 @@ const CommuteCargoPickupNavigation = () => {
       console.error("ユーザー情報が見つかりませんでした");
     }
   }, []);
+
+  useEffect(() => {
+    // nagoya
+    setStartPoint({ lat: 35.18028, lng: 136.90667 });
+    // tokyo
+    setEndPoint({ lat: 35.68944, lng: 139.69167 });
+  }, []); // 初回レンダリング時のみ実行
 
   const getStoredUserData = () => {
     if (typeof window !== 'undefined') {
@@ -55,7 +63,7 @@ const CommuteCargoPickupNavigation = () => {
 
   const handleArrived = () => {
     console.log('Driver has arrived at pickup location');
-    router.push("/arrived");
+    router.push("/driver-arrived");
   };
 
   // Mock vehicles details
@@ -70,7 +78,7 @@ const CommuteCargoPickupNavigation = () => {
         <h2 className="text-xl font-bold flex-grow">Navigate to Pickup</h2>
       </CardHeader>
       <CardContent className="p-4 flex-grow flex flex-col">
-        <div className="bg-gray-100 rounded-lg p-2 mb-4">
+        <div className="bg-gray-100 rounded-lg p-4 mb-4">
           <h3 className="font-semibold mb-2 flex items-center">
             <MapPin className="h-5 w-5 mr-2 text-red-500" />
             Pickup Location
@@ -78,24 +86,19 @@ const CommuteCargoPickupNavigation = () => {
           <p>{UserPickup.pickupLocation}</p>
         </div>
 
-        <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-        language="en"
-        region="US"
-        >
-          <Map originCity="Stamford, CT" destinationCity="New York City" />
-        </LoadScript>
-
-        <div className="bg-green-100 rounded-lg p-4 mb-4">
-          <h3 className="font-semibold mb-2">Customer Details</h3>
-          <p>{user.name}</p>
-          <div className="flex items-center mt-2">
-            <Phone className="h-5 w-5 mr-2 text-green-500" />
-            <a href={`tel:${pickupDetails.phoneNumber}`} className="text-blue-500 underline">
-              {pickupDetails.phoneNumber}
-            </a>
+          <MapComponent start={startPoint} end={endPoint} />
+          
+          <div className="bg-green-100 rounded-lg p-4 mb-4">
+            <h3 className="font-semibold mb-2">Customer Details</h3>
+            <p>{user.name}</p>
+            <div className="flex items-center mt-2">
+              <Phone className="h-5 w-5 mr-2 text-green-500" />
+              <a href={`tel:${pickupDetails.phoneNumber}`} className="text-blue-500 underline">
+                {pickupDetails.phoneNumber}
+              </a>
+            </div>
           </div>
-        </div>
+        
 
         <Button 
           onClick={handleArrived}
--- frontend/src/components/maps/open-maps.tsx
+++ frontend/src/components/maps/open-maps.tsx
@@ -1,16 +1,18 @@
 import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
 import { Clock, Navigation } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
-const DefaultIcon = L.icon({
-  iconUrl: require('leaflet/dist/images/marker-icon.png'), // 画像のパス
-  iconSize: [25, 41], // アイコンのサイズ
-  iconAnchor: [12, 41], // アイコンのアンカー
-  popupAnchor: [1, -34], // ポップアップの位置
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'), // シャドウの画像
-});
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
+})
 
 
 interface RoutePoint {
@@ -84,33 +86,34 @@ const MapComponent: React.FC<MapComponentProps> = ({ start, end }) => {
   }, [start, end]);
 
   return (
-    <div style={{ height: '50vh' }}> {/* 画面全体の高さを確保 */}
-      <MapContainer
-        center={[35.6895, 139.6917]} // Tokyo
-        zoom={13}
-        style={{ height: '90%', width: '100%' }} // 90%の高さを指定
-      >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+    <div className="flex flex-col space-y-1  mb-4" style={{ height: '55vh' }}> {/* 画面全体の高さを確保 */}
+      <div className="flex-grow bg-gray-200 rounded-lg mb-4 relative" style={{ height: '80%' }}>
+        <MapContainer
+          center={[35.6895, 139.6917]} // Tokyo
+          zoom={13}
+          style={{ height: '100%', width: '100%' }} // 90%の高さを指定
+        >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-        {start && (
-          <Marker position={[start.lat, start.lng]} icon={DefaultIcon}>
-            <Popup>出発地点</Popup>
-          </Marker>
-        )}
-        {end && (
-          <Marker position={[end.lat, end.lng]} icon={DefaultIcon}>
-            <Popup>到着地点</Popup>
-          </Marker>
-        )}
-        <RouteDisplay route={route} />
-      </MapContainer>
-
-      <div className="bg-blue-100 rounded-lg p-3 mb-4" style={{ height: '20%' }}> {/* なぜか残りの20%でうまく描画される */}
+          {start && (
+            <Marker position={[start.lat, start.lng]} >
+              <Popup>出発地点</Popup>
+            </Marker>
+          )}
+          {end && (
+            <Marker position={[end.lat, end.lng]}>
+              <Popup>到着地点</Popup>
+            </Marker>
+          )}
+          <RouteDisplay route={route} />
+        </MapContainer>
+      </div>
+      <div className="bg-blue-100 rounded-lg p-4 mb-4" style={{ height: '20%' }}> {/* なぜか残りの20%でうまく描画される */}
         <div className="flex justify-between items-center mb-2">
-          <div className="flex items-center">
+          <div className="flex items-center">  
             <Navigation className="h-5 w-5 mr-2 text-blue-500" />
             <span className="font-semibold">Distance</span>
           </div>
